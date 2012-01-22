978026c0e707a55d76f45eb4828a67593ab68d28
Justin Anthony Ball <justinball9709@gmail.com>
2012-01-22 16:13:39 -0500

    Added files

---
+++ Notes_Javascript.js
@@ -0,0 +1,111 @@
+//Author: Justin Ball
+//January 19, 2012
+//Project 3 Assignment
+//Deliverable 3
+//Wireless Industry
+
+//Variables
+var TotalPhones = 100,
+
+var TotalAccessories = 100,
+// Boolean Condition
+	if [TotalPhones = TotalAccessories]	{
+	console.log("We have a lot of phones and accessories to sell.");
+	}else {
+	console.log("We have to purchase more inventory.");
+	};
+
+//Number
+var Motorola = [ "Droid X", "Droid Razr", "Droid 3"],
+var Timetakestoactivate = [ 2, 5, 6, 8];
+
+var Activate	= function () {
+//String Property and Method 
+	var Phone = Motorola[Activation],
+	Time = Timetakestoactivate[Activation];
+
+	console.log( "The Motorola" + Motorola + "takes" + Timetakestoactivate + "minutes to activate.");
+	//For Loop
+	for [var initial = 0; minutesleft < Timetakestoactivate; minutes == 10]	{
+		var MinutesComplete = Time - minutesleft;
+
+		console.log("I took" + initial + "to get everything set up" + MinutesComplete + "and working"); 
+		}
+		console.log("Here is the new" + Phone + "left.");
+
+	for (var Talktime = 0; Talktime < TotalPhones.length; Talktime++) {
+	SellAcc();
+
+	var cla = 0;
+	console.log("We got some car chargers in" + cla + "was sold this week");
+	//while loop
+	while [cla > 0] {
+	console.log("These car chargers need to be sold"); cla++;
+	};
+	}
+//Array and ouput.. Local Variables
+var Locations = [ "Atlanta", "New York", "Tampa" ], InvLocaton, Stores;
+console.log("This location gets most inventory." InvLocation + InvLocation.length;
+console.log(InvLocation);
+console.log(" Atlanta send out all the phones to the other markets. ");
+Locations [1] = "Is next destination";
+MainLocation = InvLocation - 1; return InvLocation;
+//Returned Values and Output
+for each [var InvLocation in InvLocation;
+console.log("Phones is being mailed.");
+};
+//Return Array // Method Mutator
+
+function PhoneToSend() {
+var Send = new Device(3);
+Send[0] = "Samsung";
+Send[1] = "LG";
+Send[2] = "Audiovox";
+Send[3] = "Motorola";
+return Send.lenth;
+}
+console.log("Phones are being sent out.");
+console.log("We need to send these phones before the holidays.");
+//Object Property and Json Property/ accessor
+var Colors = {
+Design: ["Sleek", "Bulky", "Sharp"],
+Rating: [1, 2, 3, 4],
+Colors: ["Black", "White", "Gold"]
+};
+
+console.log("This is a" + Colors.Rating[4] + "rating phone.");
+var type = true;
+if (type === true) {
+	console.log("Yes this is a great phone.");
+
+} else {
+	console.log(" The ratings on this phone needs to go up.");
+};
+// Number Conditional
+var price = true, priceHigh = 5, Cost = 7 };
+if {priceHigh + Cost} {
+	console.log("Great everything is going to plan.");
+}else {
+console.log("We got to get this together.");
+};
+
+//Arguement Number // Nested Conditional
+if (Rating < 3) {
+console.log(" Yes I'm gonna enjoy this.");
+}else {
+console.log("This isn't my favorite.");
+}else {
+if (Rating > 2) {
+console.log(" This is going to be good.");
+}else {
+console.log("Totally ready for this.")
+};
+GitHub Links
+GitHub
+About
+Blog
+Features
+Contact & Support
+Training
+GitHub Enterprise
+Site Status
